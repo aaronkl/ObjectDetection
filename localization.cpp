9cf9d5276f30772725102a9d1a6002b10d7353c7
kleinaa <kleinaa@moncheri.informatik.uni-freiburg.de>
2014-03-21 10:37:56 +0100

    refactoring

--- localization.cpp
+++ localization.cpp
@@ -6,65 +6,9 @@
  */
 
 
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/opencv.hpp>
-#include <vector>
-#include <geometry_msgs/PoseStamped.h>
-//#include <pcl_ros/point_cloud.h>
-#include <sensor_msgs/PointCloud2.h>
-// PCL specific includes
-//#include <pcl/ros/conversions.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <pcl/common/io.h>
-#include <pcl/registration/icp.h>
-#include <pcl/common/centroid.h>
-#include <pcl/common/geometry.h>
-#include <pcl/filters/filter.h>
-#include "pcl/features/normal_3d.h"
-#include <pcl/registration/transforms.h>
-#include "pcl/features/fpfh.h"
-#include <pcl/registration/registration.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
-#include "pcl/registration/ia_ransac.h"
-#include <pcl/features/fpfh_omp.h>
-#include <pcl/features/normal_3d_omp.h>
-#include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/segmentation/extract_clusters.h>
-#include <pcl/filters/passthrough.h>
+#include "localization.h"
 
-
-class Localization
-{
-
-public:
-	Localization()
-	:_model (new pcl::PointCloud<pcl::PointXYZ>)
-	{
-
-		_point_cloud_sub = _nh.subscribe("/points", 1,&Localization::point_cloud_callback, this);
-		_pub_pose = _nh.advertise<geometry_msgs::PoseStamped>("/grasp_pose", 1);
-		_pub_cloud = _nh.advertise<sensor_msgs::PointCloud2>("/final", 1);
-
-		if (pcl::io::loadPCDFile<pcl::PointXYZ> ("/home/kleinaa/devel/workspace/ObjectRecognition/Handles/model.pcd", *_model) == -1)
-		{
-			std::cerr << "Could not load model file " << std::endl;
-			exit(-1);
-		}
-
-	}
-
-	~Localization() {}
-
-
-	void point_cloud_callback(const sensor_msgs::PointCloud2ConstPtr& input)
+	void Localization::point_cloud_callback(const sensor_msgs::PointCloud2ConstPtr& input)
 	{
 		std::cout << "start localization" << std::endl;
 		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
@@ -150,23 +94,6 @@ public:
 		//		}
 
 	}
-
-private:
-	ros::NodeHandle _nh;
-	pcl::PointCloud<pcl::PointXYZ>::Ptr _model;
-	ros::Subscriber _point_cloud_sub;
-	ros::Publisher _pub_pose;
-	ros::Publisher _pub_cloud;
-};
-
-int main(int argc, char** argv)
-{
-	ros::init(argc, argv, "Localization");
-	Localization loc;
-	ros::spin();
-	return (0);
-}
-
 
 
 
